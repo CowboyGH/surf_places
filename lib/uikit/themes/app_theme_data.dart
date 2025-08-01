d46489a97424b40e8a6bcb7c29771d7e8b1d3c19
CowboyGH <kocergindenis81@gmail.com>
2025-08-01 17:22:51 +0700

    add: implement uikit

---
+++ lib/uikit/themes/app_theme_data.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:surf_places/uikit/themes/colors/app_color_theme.dart';
+import 'package:surf_places/uikit/themes/text/app_text_theme.dart';
+
+abstract class AppThemeData {
+  static const _lightColorTheme = AppColorTheme.light();
+  static const _darkColorTheme = AppColorTheme.dark();
+  static final _textTheme = AppTextTheme.base();
+
+  static final lightTheme = ThemeData(
+    extensions: [_lightColorTheme, _textTheme],
+    brightness: Brightness.light,
+    splashFactory: NoSplash.splashFactory,
+    primaryColor: _lightColorTheme.neutralWhite,
+    colorScheme: ColorScheme.light(),
+    scaffoldBackgroundColor: _lightColorTheme.scaffold,
+    appBarTheme: AppBarTheme(
+      backgroundColor: _lightColorTheme.scaffold,
+      elevation: 0,
+      surfaceTintColor: Colors.transparent,
+      iconTheme: IconThemeData(color: _lightColorTheme.neutralWhite),
+      titleTextStyle: _textTheme.subtitle.copyWith(
+        color: _lightColorTheme.neutralWhite,
+      ),
+    ),
+    dividerTheme: DividerThemeData(color: _lightColorTheme.divider),
+    sliderTheme: SliderThemeData(
+      trackHeight: 2,
+      activeTrackColor: _lightColorTheme.accent,
+      inactiveTrackColor: _lightColorTheme.inactive,
+      activeTickMarkColor: Colors.transparent,
+      inactiveTickMarkColor: Colors.transparent,
+      thumbColor: _lightColorTheme.neutralWhite,
+      overlayShape: SliderComponentShape.noOverlay,
+      tickMarkShape: SliderTickMarkShape.noTickMark,
+      showValueIndicator: ShowValueIndicator.never,
+      padding: EdgeInsets.zero,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      unselectedItemColor: _lightColorTheme.textPrimary,
+      selectedItemColor: _lightColorTheme.textPrimary,
+      selectedLabelStyle: _textTheme.superSmallMedium,
+      unselectedLabelStyle: _textTheme.superSmallMedium,
+    ),
+  );
+
+  static final darkTheme = ThemeData(
+    extensions: [_darkColorTheme, _textTheme],
+    brightness: Brightness.dark,
+    splashFactory: NoSplash.splashFactory,
+    colorScheme: ColorScheme.dark(),
+    primaryColor: _darkColorTheme.scaffold,
+    scaffoldBackgroundColor: _darkColorTheme.scaffold,
+    appBarTheme: AppBarTheme(
+      backgroundColor: _darkColorTheme.scaffold,
+      elevation: 0,
+      surfaceTintColor: Colors.transparent,
+      iconTheme: IconThemeData(color: _darkColorTheme.neutralWhite),
+      titleTextStyle: _textTheme.subtitle.copyWith(
+        color: _darkColorTheme.neutralWhite,
+      ),
+    ),
+    dividerTheme: DividerThemeData(color: _darkColorTheme.divider),
+    sliderTheme: SliderThemeData(
+      trackHeight: 2,
+      activeTrackColor: _darkColorTheme.accent,
+      inactiveTrackColor: _darkColorTheme.inactive,
+      activeTickMarkColor: Colors.transparent,
+      inactiveTickMarkColor: Colors.transparent,
+      thumbColor: _darkColorTheme.neutralWhite,
+      overlayShape: SliderComponentShape.noOverlay,
+      tickMarkShape: SliderTickMarkShape.noTickMark,
+      showValueIndicator: ShowValueIndicator.never,
+      padding: EdgeInsets.zero,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      unselectedItemColor: _darkColorTheme.textPrimary,
+      selectedItemColor: _darkColorTheme.textPrimary,
+      selectedLabelStyle: _textTheme.superSmallMedium,
+      unselectedLabelStyle: _textTheme.superSmallMedium,
+    ),
+  );
+}
---
+++ lib/uikit/themes/colors/app_colors.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: member-ordering
+
+import 'package:flutter/material.dart';
+
+/// {@template app_colors.class}
+/// Цвета приложения
+/// {@endtemplate}
+abstract class AppColors {
+  /// Цвета из макета
+  static const colorWhite = Color(0xFFFFFFFF);
+  static const colorBackground = Color(0xFFF5F5F5);
+  static const colorSecondary = Color(0xFF3B3E5B);
+  static const colorSecondary2 = Color(0xFF7C7E92);
+  static const colorInactiveBlack = Color.fromRGBO(124, 126, 146, 0.56);
+  static const colorPicker = Color(0xFF8CC152);
+
+  static const colorWhiteGreen = Color(0xFF4CAF50);
+  static const colorWhiteYellow = Color(0xFFFBC02D);
+  static const colorWhiteError = Color(0xFFEF4343);
+  static const colorWhiteMain = Color(0xFF252849);
+
+  static const colorBlackGreen = Color(0xFF6ADA6F);
+  static const colorBlackYellow = Color(0xFFFFE769);
+  static const colorBlackError = Color(0xFFCF2A2A);
+  static const colorBlackDark = Color(0xFF1A1A20);
+  static const colorBlackMain = Color(0xFF21222C);
+}
