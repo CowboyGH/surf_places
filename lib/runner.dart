d06d9536b7b12030fe59f7a1e6297bac5bd21749
CowboyGH <kocergindenis81@gmail.com>
2025-08-01 16:54:42 +0700

    refactor: Update main entry point for Flutter app
    - Replace the main app structure with a direct call to the runner
    - Remove unnecessary MainApp class and its build method

---
+++ lib/runner.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:surf_places/features/app.dart';
+
+void run() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const SurfPlaces());
+}
