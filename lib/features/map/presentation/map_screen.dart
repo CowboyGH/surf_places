474f0b880c1a365124866ec5cdf2e2832093119d
CowboyGH <kocergindenis81@gmail.com>
2025-08-12 09:57:53 +0700

    feat: refactor MapScreen to use NetworkCubit for connectivity state management

--- lib/features/map/presentation/map_screen.dart
+++ lib/features/map/presentation/map_screen.dart
@@ -1,13 +1,22 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:surf_places/assets/strings/app_strings.dart';
+import 'package:surf_places/features/common/cubit/network_cubit.dart';
 import 'package:surf_places/features/common/presentation/widgets/app_search_bar.dart';
+import 'package:surf_places/features/common/presentation/widgets/load_error_widget.dart';
 import 'package:surf_places/features/common/presentation/widgets/service_unavailable_widget.dart';
 import 'package:surf_places/uikit/themes/colors/app_color_theme.dart';
 import 'package:surf_places/uikit/themes/text/app_text_theme.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     final textTheme = AppTextTheme.of(context);
@@ -29,7 +38,19 @@ class MapScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: SafeArea(child: Center(child: ServiceUnavailableWidget())),
+      body: SafeArea(
+        child: Center(
+          child: BlocBuilder<NetworkCubit, ConnectivityResult>(
+            builder: (context, state) {
+              if (state == ConnectivityResult.none) {
+                return LoadErrorWidget();
+              } else {
+                return ServiceUnavailableWidget();
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
 }
